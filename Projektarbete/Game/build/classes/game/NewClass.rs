4476617395ffe2ed7a2d7820d47742ef5e262fc1
felix.hallqvist <felix.hallqvist@67a97ffd-728a-db40-ad6d-020432d08786>
2010-10-12 10:11:52 +0000

    Fixed some folder structure, where java project ended up inside another java project.
    Also made separate folders for feha and alni in the logbook folder. Might have some being shared later on though, not sure.
    Removed a lil stuff aswell.

--- Projektarbete/Game/build/classes/game/NewClass.rs
+++
@@ -1,7 +0,0 @@
-game.TestGraphicsPanel$DownAction
-game.TestGraphicsPanel$UpAction
-game.TestGraphicsPanel
-game.TestGraphicsPanel$LeftAction
-game.TestGraphicsPanel$RightAction
-game.NewClass
-game.TestGamePanel
