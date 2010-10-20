3269f5ae5658fa5abcdc85eeb39248754a482b46
felix.hallqvist <felix.hallqvist@67a97ffd-728a-db40-ad6d-020432d08786>
2010-10-20 16:02:44 +0000

    I made visible objects work with BasePhysics. I also updated a bit of the physics in basephysics.

--- trunk/Projektarbete/src/projektarbete/BasePhysics.java
+++ trunk/Projektarbete/src/projektarbete/BasePhysics.java
@@ -30,7 +30,15 @@ public class BasePhysics {
     double[] pos;
     double xVel;
     double yVel;
-    double friction;
+    double[] vel;
+    double xAcc;
+    double yAcc;
+    double[] acc;
+    double mass;
+    double airFriction;
+    double gravity;
+    boolean gravityFlag;
+    double deltaTime;
 
     VisibleObject visibleObject;
 
@@ -54,11 +62,25 @@ public class BasePhysics {
 
         xVel = 0;
         yVel = 0;
+        vel = new double[2];
+        vel[0] = xVel;
+        vel[1] = yVel;
 
-        friction = 0.75;
+        xAcc = 0;
+        yAcc = 0;
+        acc = new double[2];
+        acc[0] = xAcc;
+        acc[1] = yAcc;
+
+        mass = 100;
+        airFriction = 0.5;
+        gravity = 9.82;
+        gravityFlag = true;
+
+        deltaTime = ((double)20)/((double)1000);
 
         physicsSimulate();
-
+        
     }
 
     private void initTesting() {
@@ -69,9 +91,10 @@ public class BasePhysics {
 
     public void updateGraphic() {
 
-        visibleObject.x = (int)xPos;
-        visibleObject.y = (int)yPos;
-        
+        if (visibleObject != null) {
+            visibleObject.setPos(xPos, yPos);
+        }
+
     }
 
     public void physicsSimulate() {
@@ -81,35 +104,55 @@ public class BasePhysics {
             timerStarted = true;
         }
 
-        xVel-= friction * xVel;
-        yVel-= friction * yVel;
+        if (gravityFlag) {
+            yAcc-= gravity * deltaTime;
+        }
 
-        xPos+= xVel;
-        yPos+= yVel;
+        //Velocity and accerelation
+        xAcc-= (airFriction * xVel)/mass;
+        yAcc-= (airFriction * yVel)/mass;
+        
+        xVel+= xAcc;
+        yVel+= yAcc;
+        vel[0] = xVel;
+        vel[1] = yVel;
+        
+        xAcc = 0;
+        yAcc = 0;
+        acc[0] = xAcc;
+        acc[1] = yAcc;
 
+        //Position
+        xPos+= xVel * deltaTime;
+        yPos+= -yVel * deltaTime;
+        pos[0]+= xPos;
+        pos[1]+= yPos;
+        System.out.println(yVel);
+        
         updateGraphic();
 
     }
-
+    
     public void move(double x, double y) {
 
         xPos+= x;
         yPos+= y;
 
-        pos[0]+= x;
-        pos[1]+= y;
+        pos[0]+= xPos;
+        pos[1]+= yPos;
 
         updateGraphic();
 
     }
 
+    //Position
     public void setPos(double x, double y) {
 
         xPos = x;
         yPos = y;
 
-        pos[0] = x;
-        pos[1] = y;
+        pos[0] = xPos;
+        pos[1] = yPos;
 
         updateGraphic();
 
@@ -120,5 +163,59 @@ public class BasePhysics {
         return pos;
 
     }
-    
+
+    //Velocity
+    public void setVel(double x, double y) {
+
+        xVel = x;
+        yVel = y;
+
+        vel[0] = xVel;
+        vel[1] = yVel;
+        
+    }
+
+    public double[] getVel() {
+
+        return vel;
+
+    }
+
+    //Accerelation and force.
+    public void applyForce(double x, double y) {
+
+        xAcc+= x / mass;
+        xAcc+= y / mass;
+
+        acc[0] = xAcc;
+        acc[1] = yAcc;
+
+    }
+
+    public void AddAcc(double x, double y) {
+
+        xAcc+= x;
+        xAcc+= y;
+
+        acc[0] = xAcc;
+        acc[1] = yAcc;
+
+    }
+
+    public void setAcc(double x, double y) {
+
+        xAcc = x;
+        xAcc = y;
+
+        acc[0] = xAcc;
+        acc[1] = yAcc;
+
+    }
+
+    public double[] getAcc() {
+
+        return acc;
+
+    }
+
 }
