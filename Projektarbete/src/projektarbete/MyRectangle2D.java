256de5b2d2fd59ef11a81252602c11898ba5eaf1
felix.hallqvist <felix.hallqvist@67a97ffd-728a-db40-ad6d-020432d08786>
2010-11-15 08:51:18 +0000

    Added a rotate function to Coordinate, and created a rectangle class (polygon that got the shape of a rectangle).

---
+++ Projektarbete/src/projektarbete/MyRectangle2D.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projektarbete;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author felix.hallqvist
+ */
+public class MyRectangle2D extends MyPolygon {
+
+    protected double width;
+    protected double height;
+
+
+    public MyRectangle2D(VisibleObject visibleObject) {
+
+        super(visibleObject);
+
+        System.out.println("Rectangle Initializing");
+
+        initComponents();
+
+        System.out.println("Rectangle Initialized");
+
+    }
+
+    private void initComponents() {
+        width=12;
+        height=12;
+
+        points = new Coordinate[]{
+            new Coordinate(-width, -height),
+            new Coordinate(width, -height),
+            new Coordinate(width, height),
+            new Coordinate(-width, height)};
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        super.draw(g);
+        
+        //g.fillPolygon(polygon);
+    }
+
+}
