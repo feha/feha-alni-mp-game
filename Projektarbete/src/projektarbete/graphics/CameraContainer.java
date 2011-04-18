03418fbdb7335b78167e7ea6ae61a213278cac6f
felix.hallqvist <felix.hallqvist@67a97ffd-728a-db40-ad6d-020432d08786>
2011-04-18 20:34:29 +0000

    Created a CameraContainer class, which is sort of like a class which stuff can use as a camera, and then when the cameras container is set to the instance, the camera will use the position and such. The container can use a VisibleObject as a parent, making it possible for it to follow a VisibleObject with the Camera. Also added getwidth and heigth for MyImage and lets TestUserObject use the container (and container uses the mouse for a slight offset from the VisibleObject.

---
+++ Projektarbete/src/projektarbete/graphics/CameraContainer.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projektarbete.graphics;
+
+import projektarbete.Coordinate;
+
+/**
+ *
+ * @author felix.hallqvist
+ */
+public class CameraContainer {
+
+    public CameraContainer() {
+
+    }
+
+    VisibleObject parent;
+    Coordinate position;
+    public Coordinate offset;
+    double angle;
+    Coordinate scale;
+
+    public void setParent(VisibleObject newParent) {
+        parent = newParent;
+    }
+
+    public void setPos(Coordinate newPos) {
+        position = newPos;
+    }
+
+    public void setAng(double newAng) {
+        angle = newAng;
+    }
+
+    public void setScale(Coordinate newScale) {
+        scale = newScale;
+    }
+
+
+    public VisibleObject getParent() {
+        return parent;
+    }
+
+    public Coordinate getPos() {
+        return parent != null ? parent.position.add(offset) : position.add(offset);
+    }
+
+    public double getAng() {
+        return parent != null ? parent.angle : angle;
+    }
+
+    public Coordinate getScale() {
+        return scale;
+    }
+
+}
